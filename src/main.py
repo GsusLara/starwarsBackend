6eb744e39495a892767c959e2cfc815ee769d6f8
Jesus Lara <jelara99@gmail.com>
2021-04-17 04:32:56 +0000

    avance bd y get

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Characters, Planets, Favorites
 #from models import Person
 
 app = Flask(__name__)
@@ -31,13 +31,17 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def listarUsuarios():
+    users = User.query.all()
+    request = list(map(lambda user:user.serialize(),users))    
+    return jsonify(request), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/planets', methods=['GET'])
+def getPlanets():
+    planeta = Planets.query.all()
+    request = list(map(lambda planeta:planeta.serialize(),planeta))    
+    return jsonify(request), 200
 
-    return jsonify(response_body), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,6 +2,47 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class Characters(db.Model):
+    id = db.Column(db.Integer, primary_key=True)   
+    name=db.Column(db.String(50), nullable=False)
+    height=db.Column(db.Float)
+    mass=db.Column(db.Integer)
+    gender=db.Column(db.String(50))
+    homeworld=db.Column(db.String(50))
+    image=db.Column(db.String(50))
+    born=db.Column(db.Integer)
+    died=db.Column(db.Integer)
+    species=db.Column(db.String(50))
+    cybernetics=db.Column(db.String(50))
+
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)   
+    name=db.Column(db.String(50), nullable=False)
+    diameter=db.Column(db.Integer)
+    rotation_period=db.Column(db.Integer)
+    orbital_period=db.Column(db.Integer)
+    surface_water=db.Column(db.Integer)
+    gravity=db.Column(db.String(50))
+    terrain=db.Column(db.String(50))
+    population=db.Column(db.Integer)
+
+    # def __repr__(self):
+    #     return '<Planets %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "nombre": self.name,
+            "rotation period": self.rotation_period,
+            # do not serialize the password, its a security breach
+        }
+
+class Favorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    users_id = db.Column(db.Integer, db.ForeignKey('User.id'))
+    # planets_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
+    # characters_id = db.Column(db.Integer, db.ForeignKey('characters.id'))  
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
