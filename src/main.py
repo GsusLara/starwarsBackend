29561f0e3c75e647bfb2b98db6e938cdf1e829fd
Jesus Lara <jelara99@gmail.com>
2021-04-20 04:28:27 +0000

    endpoint finalizados

--- src/main.py
+++ src/main.py
@@ -30,11 +30,35 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def listarUsuarios():
-    users = User.query.all()
-    request = list(map(lambda user:user.serialize(),users))    
-    return jsonify(request), 200
+@app.route('/login', methods=["POST"])
+#se debe enviar la data como objeto
+def login():
+    username = request.json["email"]
+    password = request.json["password"]
+    if not username:
+        return jsonify({"error": "user or password not foundd"}), 410
+    if not password:
+        return jsonify({"error": "user or password not found"}), 410
+    user = User.query.filter_by(email=username).first()
+    if not user:
+        return jsonify({"error": "user or password not found"}), 410
+    elif user.password != password:
+        return jsonify({"error": "user or password not found"}), 410
+    else:  
+        return jsonify("inicio correcto"), 200
+
+@app.route('/user', methods=['POST']) 
+#se debe enviar la data como objeto
+def add_user():
+    data = request.get_json()
+    validacion = User.query.filter_by(email=data["email"]).first()
+    if validacion is None:
+        user1 = User(email=data["email"],password=data["password"],is_active=True)
+        db.session.add(user1)
+        db.session.commit()
+        return jsonify("Message : Se adiciono un usuario!"),200
+    else:
+        return jsonify("Message : el usuario ya existe!"),422
 
 @app.route('/planets', methods=['GET'])
 def getPlanets():
@@ -42,6 +66,45 @@ def getPlanets():
     request = list(map(lambda planeta:planeta.serialize(),planeta))    
     return jsonify(request), 200
 
+@app.route('/planets/<int:position>', methods=['GET'])
+def listPlanetsid(position):
+    planeta = Planets.query.filter_by(id=position).first()
+    request = planeta.serialize()
+    return jsonify(request), 200
+
+@app.route('/characters', methods=['GET'])
+def getCharacters():
+    personaje = Characters.query.all()
+    request = list(map(lambda person:person.serialize(),personaje))    
+    return jsonify(request), 200
+
+@app.route('/characters/<int:position>', methods=['GET'])
+def listCharactersid(position):
+    character = Characters.query.filter_by(id=position).first()
+    request = character.serialize()
+    return jsonify(request), 200
+
+@app.route('/favorites/<int:user>', methods=['GET'])
+def getFavorites(user):
+    favorito = Favorites.query.filter_by(user_id=user).all()
+    request = list(map(lambda todo:todo.serialize(),favorito)) 
+    return jsonify(request), 200
+
+@app.route('/favorites/<int:id>', methods=['DELETE'])
+def delFavorites(id):
+    favorito = Favorites.query.get(id)
+    db.session.delete(favorito)
+    db.session.commit()
+    return jsonify("favorito eliminado"), 200
+
+@app.route('/favorites', methods=['POST'])
+#se debe enviar la data como objeto
+def add_newFavorite():
+    data = request.get_json()
+    favorito = Favorites(name=data["name"],user_id=data["user id"])
+    db.session.add(favorito)
+    db.session.commit()
+    return jsonify("favorito agregado"), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -9,11 +9,26 @@ class Characters(db.Model):
     mass=db.Column(db.Integer)
     gender=db.Column(db.String(50))
     homeworld=db.Column(db.String(50))
-    image=db.Column(db.String(50))
+    image=db.Column(db.String(255))
     born=db.Column(db.Integer)
     died=db.Column(db.Integer)
     species=db.Column(db.String(50))
     cybernetics=db.Column(db.String(50))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "height": self.height,
+            "mass": self.mass,
+            "gender":self.gender,
+            "homeworld":self.homeworld,
+            "image":self.image,
+            "born":self.born,
+            "died":self.died,
+            "species":self.species,
+            "cybernetics":self.cybernetics
+        }
 
 class Planets(db.Model):
     id = db.Column(db.Integer, primary_key=True)   
@@ -26,28 +41,26 @@ class Planets(db.Model):
     terrain=db.Column(db.String(50))
     population=db.Column(db.Integer)
 
-    # def __repr__(self):
-    #     return '<Planets %r>' % self.username
-
     def serialize(self):
         return {
             "id": self.id,
-            "nombre": self.name,
+            "name": self.name,
+            "diameter": self.diameter,
             "rotation period": self.rotation_period,
-            # do not serialize the password, its a security breach
+            "orbital_period":self.orbital_period,
+            "surface_water":self.surface_water,
+            "gravity":self.gravity,
+            "terrain":self.terrain,
+            "population":self.population
         }
 
-class Favorites(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    users_id = db.Column(db.Integer, db.ForeignKey('User.id'))
-    # planets_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
-    # characters_id = db.Column(db.Integer, db.ForeignKey('characters.id'))  
-
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favorites = db.relationship("Favorites")
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -58,3 +71,18 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Favorites(db.Model):
+    __tablename__ = "favorites"
+    id = db.Column(db.Integer, primary_key=True)
+    name =  db.Column(db.String(50))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user id":self.user_id,
+            "name": self.name,
+            # do not serialize the password, its a security breach
+        }
+
