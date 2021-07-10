dffb48656b0786f98fbcdc8a80ac826419b704e9
Jesus Lara <jelara99@gmail.com>
2021-07-09 20:27:21 -0600

    postgreSQL redy deploy

--- src/main.py
+++ src/main.py
@@ -18,7 +18,7 @@ import datetime
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING').replace("postgres://","postgresql://")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("postgres://","postgresql://")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 MIGRATE = Migrate(app, db)
 db.init_app(app)
